f5e70c873ed5cade827957420bddfe2f5d2c0148
thuraiselvaraj@gmail.com <thuraiselvaraj@gmail.com>
2021-02-13 12:32:52 -0500

    added custom chat queue

--- Chat/serv.js
+++ Chat/serv.js
@@ -1,34 +1,52 @@
-const express=require("express")
-const bodyParser=require("body-parser")
-const app=express()
-const {client}=require("../Postgres_Models/connection")
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+const events=require("events");
+const queue = new events.EventEmitter();
+
+// const {client}=require("../Postgres_Models/connection")
 const server = require('http').createServer(app)
-const redis = require("redis"),
-    client = redis.createClient({
-            host:"localhost",
-            port:6379
-        });
+// const redis = require("redis"),
+//     client = redis.createClient({
+//             host:"localhost",
+//             port:6379
+//         });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 app.use("/", express.static("static"));
 
-app.get("/chat/", (req, res) => {
-  console.log("inside chat")
-        res.writeHead(200, {
-          "Content-Type": "text/event-stream",
-          "Cache-Control": "no-cache",
-          "Connection": "keep-alive"
-        });      
-})  
+// app.get("/chat/", (req, res) => {
+//         res.writeHead(200, {
+//           "Content-Type": "text/event-stream",
+//           "Cache-Control": "no-cache",
+//           "Connection": "keep-alive"
+//         });      
+// })  
 
-app.post("/write",(req,res)=>{
-  client.map(e=>{
-    e.write(`data:${JSON.stringify(req.body)}\n\n`)
-    console.log(client.length)
-  })
-  res.end()
+app.post("/write/:_id",(req,res)=>{
+    // console.log(JSON.stringify(req.body))
+    queue.emit(req.params._id,`data:${JSON.stringify(req.body)}\n\n`)
+    res.end()
+})
+
+app.get("/chat/:_id/:_rid",(req,res)=>{
+    function anon(data){
+        console.log(data)
+        res.write(data)
+    }
+    res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive"
+    });   
+    queue.on(req.params._id,anon)
+
+    req.on("close",()=>{
+        console.log("closes")
+        queue.removeListener(req.params._rid,anon)
+    })
 })
 
 
---
+++ Tests/eventh.js
@@ -0,0 +1,30 @@
+const events=require("events")
+const conn=require("../Postgres_Models/connection.js").client 
+var em = new events.EventEmitter();
+
+//Subscribe for FirstEvent
+// em.on('FirstEvent', function (data) {
+//     console.log('First subscriber: ' + data);
+// });
+
+// // Raising FirstEvent
+// em.emit('FirstEvent', 'This is my first Node.js event emitter example.');
+limit =20;
+function sample(data) {
+    console.log('First subscriber: ' + data);
+}
+for(let i=0;i<limit;i++){
+    em.on(i, sample);
+}
+
+for(let i=0;i<20;i++){
+    em.emit(i,200);
+}
+
+for(let i=0;i<limit;i++){
+    em.removeListener(i, sample);
+}
+
+for(let i=0;i<limit+10;i++){
+    em.on(i, sample);
+}
